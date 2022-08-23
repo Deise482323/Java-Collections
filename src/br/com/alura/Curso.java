8b938f4ec459300377076361f96d98fcbbdd48e0
Deise482323 <deise0509@gmail.com>
2022-08-23 12:02:04 -0300

    Aulas 03

--- src/br/com/alura/Curso.java
+++ src/br/com/alura/Curso.java
@@ -1,6 +1,7 @@
 package br.com.alura;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Curso {
@@ -22,6 +23,13 @@ public class Curso {
     }
 
     public List<Aula> getAulas() {
-        return aulas;
+        return Collections.unmodifiableList(aulas);
+    }
+
+    public void adiciona(Aula aula) {
+        this.aulas.add(aula);
+
     }
 }
+
+
--- src/br/com/alura/TestaCurso.java
+++ src/br/com/alura/TestaCurso.java
@@ -4,8 +4,16 @@ import java.util.List;
 
 public class TestaCurso {
     public static void main(String[] args) {
-        Curso javaColecoes = new Curso("Dominando as coleções do java","Paulo Silveira");
+        Curso javaColecoes = new Curso("Dominando as coleções do java", "Paulo Silveira");
         List<Aula> aulas = javaColecoes.getAulas();
+        System.out.println(aulas);
+
+        System.out.println("-------------------------------------");
+
+        javaColecoes.adiciona(new Aula("trabalhando com ArrayList", 21 ));
+        javaColecoes.adiciona(new Aula("Criando aula", 20 ));
+        javaColecoes.adiciona(new Aula("Modelando coleções", 22 ));
+
         System.out.println(aulas);
     }
 }
